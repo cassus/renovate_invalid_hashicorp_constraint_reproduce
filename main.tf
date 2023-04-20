56d3c6c091a1f7716ada451f26e63e8b6e80dbfb
Adam Banko <banko.adam@gmail.com>
2023-04-20 22:47:36 +0200

    init

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.54.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "tealcompass"
+  region  = "europe-west3"
+  zone    = "europe-west3-b"
+}
+
+
+module "db1_prod_socket_url" {
+  source     = "memes/secret-manager/google"
+  version    = "2.1.1" 
+  project_id = "xxxxxx"
+  id         = "x"
+  secret     = "secret"
+}
